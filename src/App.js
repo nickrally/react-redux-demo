3bd4edc3a6728bc4cd6ffdaae9887e44e9549d8e
nickrally <nickrally2009@gmail.com>
2021-03-03 23:50:03 -0700

    added ItemContainer to explain ownProps

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import store from './redux/store';
 import './App.css';
 import CakeContainer from './components/CakeContainer'
 import IceCreamContainer from './components/IceCreamContainer'
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer />
         <CakeContainer />
         <IceCreamContainer />
       </div>
     </Provider>
-    
   );
 }
-
 export default App;
