50c117b27112acdffd02d541fb13cf0ecb200b3f
nickrally <nickrally2009@gmail.com>
2021-03-03 21:54:18 -0700

    added payload to action obj

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { buyCake } from '../redux';
 
 function CakeContainer(){
+    const [number, setNumber] = useState(1);
     const numOfCakes = useSelector(state => state.cake.numOfCakes);
     const dispatch = useDispatch()
     return(
         <div>
             <h2>Number of cakes: {numOfCakes}</h2>
-            <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
+            <input type="text" value={number} onChange={ event => setNumber(event.target.value)} />
+            <button onClick={() => dispatch(buyCake(number))}>Buy {number} Cakes</button>
         </div>
     )
 }
